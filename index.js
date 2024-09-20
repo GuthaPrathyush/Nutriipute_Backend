d38a913a00ea51a1f2e49c3b71bdcba80f320ea8
Gutha Prathyush <prathyushgutha@gmail.com>
2024-09-20 21:19:58 +0530

    Added .env

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const { type } = require('os');
+const dotenv = require('dotenv');
 
-const saltRounds = 10;
-const jwtString = 'aIewEkdeiIdaeAsdeogDVscwQo';
-const port = 3000;
+dotenv.config();
 
-let databaseAdmin = 'readProducts';
-let databasePassword = 'eMPRerUNleOqrJJC';
+const saltRounds = process.env.SALT_ROUNDS;
+const jwtString = process.env.JWT_STRING;
+const port = process.env.PORT;
+
+let databaseAdmin = process.env.DATABASE_ADMIN;
+let databasePassword = process.env.DATABASE_PASSWORD;
 
 const corsOptions = {
     origin: 'https://nutriipute.vercel.app',
@@ -29,6 +33,10 @@ mongoose.connect(`mongodb+srv://${databaseAdmin}:${databasePassword}@cluster0.c6
 app.get('/', (req, res) => {
     res.send("API is running");
 });
+
+function generateOrderId() {
+    return `${Date.now()}${100+Math.floor(Math.random()*900)}`.replace(/(.{4})/g, '$1-').slice(0, -1);
+}
 
 const userSchema = mongoose.Schema({
     Name: {
@@ -70,20 +78,41 @@ const productsSchema = mongoose.Schema({
     }
 });
 
+const orderSchema = mongoose.Schema({
+    OrderId: {
+        type: String,
+        required: true
+    },
+    UserId: {
+        type: String,
+        requried: true
+    },
+    Details: {
+        type: Object,
+        required: true
+    }
+});
+
+/* Details: {
+    products: [array of products which are objects that contain productid, their ordered price]
+    address: address
+}*/
+
 const User = mongoose.model("Users", userSchema);
 const Products = mongoose.model('Products', productsSchema);
+const Order = mongoose.model('Orders', orderSchema);
 
 
 app.post('/register', async (req, res) => {
-    let check = await User.findOne({Email: req.body.Email});
-    if(check) {
-        return res.status(400).json({success: false, errors: "Existing User"});
+    let check = await User.findOne({ Email: req.body.Email });
+    if (check) {
+        return res.status(400).json({ success: false, errors: "Existing User" });
     }
     let Cart = {};
     let Address = [];
     let Orders = {};
     const hashedPassword = await bcrypt.hash(req.body.Password, saltRounds).catch(e => {
-        return res.status(500).json({success: false, errors: "Password Error"});
+        return res.status(500).json({ success: false, errors: "Password Error" });
     });
     const user = new User({
         Name: req.body.Name,
@@ -94,29 +123,29 @@ app.post('/register', async (req, res) => {
         Orders
     });
     await user.save().catch(e => {
-        return res.status(500).json({success: false, errors: "User Error"});
+        return res.status(500).json({ success: false, errors: "User Error" });
     });
-    res.json({success: true});
+    res.json({ success: true });
 });
 
 app.post('/login', async (req, res) => {
     const emailF = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     const passF = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W\_])[A-Za-z\d\W\_]+$/;
-    if(req.body.Email.trim() === '' || req.body.Password.trim() === '') {
-        return res.status(400).json({success: false, errors: "Empty fields"});
+    if (req.body.Email.trim() === '' || req.body.Password.trim() === '') {
+        return res.status(400).json({ success: false, errors: "Empty fields" });
     }
-    else if(!emailF.test(req.body.Email)) {
-        return res.status(400).json({success: false, errors: "Invalid email!"});
+    else if (!emailF.test(req.body.Email)) {
+        return res.status(400).json({ success: false, errors: "Invalid email!" });
     }
-    else if(!passF.test(req.body.Password)){
-        return res.status(400).json({success: false, errors: "Invalid Password!"});
+    else if (!passF.test(req.body.Password)) {
+        return res.status(400).json({ success: false, errors: "Invalid Password!" });
     }
     try {
         const user = await User.findOne({ Email: req.body.Email });
         if (!user) {
             return res.status(400).json({ success: false, errors: "User Not Found" });
         }
-        
+
         const isPasswordValid = await bcrypt.compare(req.body.Password, user.Password);
         if (!isPasswordValid) {
             return res.status(400).json({ success: false, errors: "Wrong Password" });
@@ -137,103 +166,103 @@ app.post('/login', async (req, res) => {
 
 app.post('/getDefaultCart', async (req, res) => {
     const token = req.body.token;
-    if(!token) {
-        return res.status(400).json({success: false, errors: 'Please pass a token'});
+    if (!token) {
+        return res.status(400).json({ success: false, errors: 'Please pass a token' });
     }
     let userId;
     try {
         const data = jwt.verify(token, jwtString);
         userId = data.user.id;
     }
-    catch(error) {
-        return res.status(401).json({success: false, errors: error});
+    catch (error) {
+        return res.status(401).json({ success: false, errors: error });
     }
-    const userData = await User.findOne({_id: userId});
-    if(!userData) {
-        return res.status(400).json({success: false, errors: "Invalid User"});
+    const userData = await User.findOne({ _id: userId });
+    if (!userData) {
+        return res.status(400).json({ success: false, errors: "Invalid User" });
     }
-    if(userData.Cart) {
-        res.json({success: true, Cart: userData.Cart});
+    if (userData.Cart) {
+        res.json({ success: true, Cart: userData.Cart });
     }
     else {
-        res.json({success: true, Cart: {}});
+        res.json({ success: true, Cart: {} });
     }
 });
 
 app.post('/getAddress', async (req, res) => {
     const token = req.body.token;
-    if(!token) {
-        return res.status(400).json({success: false, errors: 'Please pass a token'});
+    if (!token) {
+        return res.status(400).json({ success: false, errors: 'Please pass a token' });
     }
     let userId;
     try {
         const data = jwt.verify(token, jwtString);
         userId = data.user.id;
     }
-    catch(error) {
-        return res.status(401).json({success: false, errors: error});
+    catch (error) {
+        return res.status(401).json({ success: false, errors: error });
     }
-    const userData = await User.findOne({_id: userId});
-    if(!userData) {
-        return res.status(400).json({success: false, errors: "Invalid User"});
+    const userData = await User.findOne({ _id: userId });
+    if (!userData) {
+        return res.status(400).json({ success: false, errors: "Invalid User" });
     }
-    if(userData.Address) {
-        res.json({success: true, Address: userData.Address});
+    if (userData.Address) {
+        res.json({ success: true, Address: userData.Address });
     }
     else {
-        res.json({success: true, Address: []});
+        res.json({ success: true, Address: [] });
     }
 });
 
 app.post('/addAddress', async (req, res) => {
     const token = req.header('auth-token');
     const address = req.body;
-    if(!token) {
-        return res.status(400).json({success: false, errors: 'Please pass a token'});
+    if (!token) {
+        return res.status(400).json({ success: false, errors: 'Please pass a token' });
     }
     let userId;
     try {
         const data = jwt.verify(token, jwtString);
         userId = data.user.id;
     }
-    catch(error) {
-        return res.status(401).json({success: false, errors: error});
+    catch (error) {
+        return res.status(401).json({ success: false, errors: error });
     }
-    const userData = await User.findOne({_id: userId});
-    if(!userData) {
-        return res.status(400).json({success: false, errors: "Invalid User"});
+    const userData = await User.findOne({ _id: userId });
+    if (!userData) {
+        return res.status(400).json({ success: false, errors: "Invalid User" });
     }
-    if(userData.Address) {
+    if (userData.Address) {
         userData.Address.push(address);
     }
     else {
         userData.Address = [address];
     }
-    await User.findOneAndUpdate({_id: userId}, {Address: userData.Address});
-    res.json({success: true});
+    await User.findOneAndUpdate({ _id: userId }, { Address: userData.Address });
+    res.json({ success: true });
 });
 
 app.post('/editAddress', async (req, res) => {
     const token = req.header('auth-token');
     const indexToModify = req.header('index-to-modify');
     const address = req.body;
-    if(!token) {
-        return res.status(400).json({success: false, errors: 'Please pass a token'});
+    if (!token) {
+        return res.status(400).json({ success: false, errors: 'Please pass a token' });
     }
     let userId;
     try {
         const data = jwt.verify(token, jwtString);
         userId = data.user.id;
     }
-    catch(error) {
-        return res.status(401).json({success: false, errors: error});
+    catch (error) {
+        return res.status(401).json({ success: false, errors: error });
     }
-    const userData = await User.findOne({_id: userId});
-    if(!userData) {
-        return res.status(400).json({success: false, errors: "Invalid User"});
+    const userData = await User.findOne({ _id: userId });
+    if (!userData) {
+        return res.status(400).json({ success: false, errors: "Invalid User" });
     }
-    if(userData.Address) {
-        if(indexToModify < userData.Address.length) {
+    if (userData.Address) {
+        if (indexToModify < userData.Address.length) {
             userData.Address[indexToModify] = address;
         }
         else {
@@ -243,124 +272,129 @@ app.post('/editAddress', async (req, res) => {
     else {
         userData.Address = [address];
     }
-    await User.findOneAndUpdate({_id: userId}, {Address: userData.Address});
-    res.json({success: true});
+    await User.findOneAndUpdate({ _id: userId }, { Address: userData.Address });
+    res.json({ success: true });
 });
 
-app.post('/delAddress', async(req, res) => {
+app.post('/delAddress', async (req, res) => {
     const token = req.header('auth-token');
     const index = req.body.index;
-    if(!token) {
-        return res.status(400).json({success: false, errors: 'Please pass a token'});
+    if (!token) {
+        return res.status(400).json({ success: false, errors: 'Please pass a token' });
     }
     let userId;
     try {
         const data = jwt.verify(token, jwtString);
         userId = data.user.id;
     }
-    catch(error) {
-        return res.status(401).json({success: false, errors: error});
+    catch (error) {
+        return res.status(401).json({ success: false, errors: error });
     }
-    const userData = await User.findOne({_id: userId});
-    if(!userData) {
-        return res.status(400).json({success: false, errors: "Invalid User"});
+    const userData = await User.findOne({ _id: userId });
+    if (!userData) {
+        return res.status(400).json({ success: false, errors: "Invalid User" });
     }
     userData.Address = userData.Address.filter((_, i) => i != index);
-    await User.findOneAndUpdate({_id: userId}, {Address: userData.Address});
-    res.json({success: true});
+    await User.findOneAndUpdate({ _id: userId }, { Address: userData.Address });
+    res.json({ success: true });
 });
 
 app.post('/addToCart', async (req, res) => {
     const token = req.header('auth-token');
     const product_id = req.body.product_id;
-    if(!token) {
-        return res.status(400).json({success: false, errors: 'Please pass a token'});
+    if (!token) {
+        return res.status(400).json({ success: false, errors: 'Please pass a token' });
     }
     let userId;
     try {
         const data = jwt.verify(token, jwtString);
         userId = data.user.id;
     }
-    catch(error) {
-        return res.status(401).json({success: false, errors: error});
+    catch (error) {
+        return res.status(401).json({ success: false, errors: error });
     }
-    const userData = await User.findOne({_id: userId});
-    if(!userData) {
-        return res.status(400).json({success: false, errors: "Invalid User"});
+    const userData = await User.findOne({ _id: userId });
+    if (!userData) {
+        return res.status(400).json({ success: false, errors: "Invalid User" });
     }
-    if(userData.Cart) {
-        userData.Cart[product_id] = userData.Cart[product_id]? userData.Cart[product_id]+1: 1; 
+    if (userData.Cart) {
+        userData.Cart[product_id] = userData.Cart[product_id] ? userData.Cart[product_id] + 1 : 1;
     }
     else {
-        userData.Cart = {[product_id]: 1};
+        userData.Cart = { [product_id]: 1 };
     }
-    await User.findOneAndUpdate({_id: userId}, {Cart: userData.Cart});
-    res.json({success: true});
+    await User.findOneAndUpdate({ _id: userId }, { Cart: userData.Cart });
+    res.json({ success: true });
 });
 
 app.post('/removeFromCart', async (req, res) => {
     const token = req.header('auth-token');
     const product_id = req.body.product_id;
-    if(!token) {
-        return res.status(400).json({success: false, errors: 'Please pass a token'});
+    if (!token) {
+        return res.status(400).json({ success: false, errors: 'Please pass a token' });
     }
     let userId;
     try {
         const data = jwt.verify(token, jwtString);
         userId = data.user.id;
     }
-    catch(error) {
-        return res.status(401).json({success: false, errors: error});
+    catch (error) {
+        return res.status(401).json({ success: false, errors: error });
     }
-    const userData = await User.findOne({_id: userId});
-    if(!userData) {
-        return res.status(400).json({success: false, errors: "Invalid User"});
+    const userData = await User.findOne({ _id: userId });
+    if (!userData) {
+        return res.status(400).json({ success: false, errors: "Invalid User" });
     }
-    if(userData.Cart) {
-        if(userData.Cart[product_id] > 0) {
-            userData.Cart[product_id] = userData.Cart[product_id]-1;
-        } 
+    if (userData.Cart) {
+        if (userData.Cart[product_id] > 0) {
+            userData.Cart[product_id] = userData.Cart[product_id] - 1;
+        }
         else {
             delete userData.Cart[product_id];
         }
     }
-    await User.findOneAndUpdate({_id: userId}, {Cart: userData.Cart});
-    res.json({success: true});
+    await User.findOneAndUpdate({ _id: userId }, { Cart: userData.Cart });
+    res.json({ success: true });
 });
 
 app.post('/deleteFromCart', async (req, res) => {
     const token = req.header('auth-token');
     const product_id = req.body.product_id;
-    if(!token) {
-        return res.status(400).json({success: false, errors: 'Please pass a token'});
+    if (!token) {
+        return res.status(400).json({ success: false, errors: 'Please pass a token' });
     }
     let userId;
     try {
         const data = jwt.verify(token, jwtString);
         userId = data.user.id;
     }
-    catch(error) {
-        return res.status(401).json({success: false, errors: error});
+    catch (error) {
+        return res.status(401).json({ success: false, errors: error });
     }
-    const userData = await User.findOne({_id: userId});
-    if(!userData) {
-        return res.status(400).json({success: false, errors: "Invalid User"});
+    const userData = await User.findOne({ _id: userId });
+    if (!userData) {
+        return res.status(400).json({ success: false, errors: "Invalid User" });
     }
-    if(userData.Cart) {
+    if (userData.Cart) {
         delete userData.Cart[product_id];
     }
-    await User.findOneAndUpdate({_id: userId}, {Cart: userData.Cart});
-    res.json({success: true});
+    await User.findOneAndUpdate({ _id: userId }, { Cart: userData.Cart });
+    res.json({ success: true });
 });
 
-app.post('/getAllProducts', async(req, res) => {
-    const products = await Products.find({}, { Products: 1, _id: 0});
+app.post('/getAllProducts', async (req, res) => {
+    const products = await Products.find({}, { Products: 1, _id: 0 });
     // alternative is await Products.find({}).select(['-Domain', '-_id', '-__v']);
     const _products = [];
     products.forEach((element) => {
         _products.push(element.Products);
     });
-    res.json({success: true, Products: _products});
+    res.json({ success: true, Products: _products });
+});
+
+app.post('/order', async (req, res) => {
+    const token = req.header('auth-token');
+
 });
 
 // app.use('/images', express.static('upload/images'));
@@ -374,7 +408,7 @@ app.use("/", (req, res) => {
 
 
 app.listen(port, (e) => {
-    if(!e) {
+    if (!e) {
         console.log("app is listening at port 3000");
     }
     else {
